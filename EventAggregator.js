29edd236f2ea4c09ed9926f2392f5077b07762e3
Andrew Petersen <andrew.petersen15@gmail.com>
2014-06-12 09:45:30 -0500

    Initial commit

---
+++ EventAggregator.js
@@ -0,0 +1,71 @@
+var EventAggregator = function() {
+	this.eventKeys = {};
+	this.lastSubscriptionId = -1;
+};
+
+EventAggregator.prototype.on = function(key, callback) {
+	if (typeof callback === "function") {
+		if (!this.eventKeys[key]) {
+			this.eventKeys[key] = {
+				subscriptions: {}
+			};
+		}
+		var token = (++this.lastSubscriptionId).toString();
+		this.eventKeys[key].subscriptions[token] = callback;
+		return token;
+	} else {
+		return false;
+	}
+};
+
+EventAggregator.prototype.off = function(key, tokenOrCallback) {
+	if (typeof tokenOrCallback === 'function') {
+		//Callback reference was passed in so find the subscription with the matching function
+		if (this.eventKeys[key]) {
+			var eventSubscriptions = this.eventKeys[key].subscriptions;
+			var matchingId = null;
+			//foreach subscription see if the functions match and save the key if yes
+			for (var subscriptionId in eventSubscriptions) {
+				if (eventSubscriptions.hasOwnProperty(subscriptionId)) {
+					if (eventSubscriptions[subscriptionId] === tokenOrCallback) {
+						matchingId = subscriptionId;
+					}
+				}
+			}
+			if (matchingId !== null) {
+				delete eventSubscriptions[matchingId];
+			}
+		}
+	} else {
+		//Token was passed in
+		if (this.eventKeys[key] && this.eventKeys[key].subscriptions[tokenOrCallback]) {
+			delete this.eventKeys[key].subscriptions[tokenOrCallback];
+		}
+	}
+};
+
+EventAggregator.prototype.trigger = function(key) {
+	var self = this;
+	if (self.eventKeys[key]) {
+		var values = Array.prototype.slice.call(arguments, 1);
+		//If passing less than values pass them individually
+		var a1 = values[0],
+			a2 = values[1],
+			a3 = values[2];
+		//Else if passing more than 3 values group as an args array
+		if (values.length > 3) {
+			a1 = values;
+		}
+
+		var subscriptions = self.eventKeys[key].subscriptions;
+		setTimeout(function() {
+			for (var token in subscriptions) {
+				if (subscriptions.hasOwnProperty(token)) {
+					subscriptions[token](a1, a2, a3);
+				}
+			}
+		}, 0);
+	}
+};
+
+module.exports = EventAggregator;
