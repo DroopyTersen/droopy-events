7689222b50cd9f514c571802be84989a44a269b8
Schlintz <BSchlintz@rwbaird.com>
2017-05-31 13:23:12 -0500

    Added an optional instanceId parameter to base EventAggregator constructor. Allows instantiating multiple EventAggregator instances on the same page subsribed to the same event keys but remain scoped to the specified instanceId.

--- EventAggregator.js
+++ EventAggregator.js
@@ -1,17 +1,19 @@
-var EventAggregator = function() {
+var EventAggregator = function(instanceId) {
+	this.instanceId = instanceId || '';
 	this.eventKeys = {};
 	this.lastSubscriptionId = -1;
 };
 
 EventAggregator.prototype.on = function(key, callback) {
+	var compoundKey = this.instanceId ? this.instanceId + key : key;
 	if (typeof callback === "function") {
-		if (!this.eventKeys[key]) {
-			this.eventKeys[key] = {
+		if (!this.eventKeys[compoundKey]) {
+			this.eventKeys[compoundKey] = {
 				subscriptions: {}
 			};
 		}
 		var token = (++this.lastSubscriptionId).toString();
-		this.eventKeys[key].subscriptions[token] = callback;
+		this.eventKeys[compoundKey].subscriptions[token] = callback;
 		return token;
 	} else {
 		return false;
@@ -19,10 +21,11 @@ EventAggregator.prototype.on = function(key, callback) {
 };
 
 EventAggregator.prototype.off = function(key, tokenOrCallback) {
+	var compoundKey = this.instanceId ? this.instanceId + key : key;	
 	if (typeof tokenOrCallback === 'function') {
 		//Callback reference was passed in so find the subscription with the matching function
-		if (this.eventKeys[key]) {
-			var eventSubscriptions = this.eventKeys[key].subscriptions;
+		if (this.eventKeys[compoundKey]) {
+			var eventSubscriptions = this.eventKeys[compoundKey].subscriptions;
 			var matchingId = null;
 			//foreach subscription see if the functions match and save the key if yes
 			for (var subscriptionId in eventSubscriptions) {
@@ -38,15 +41,16 @@ EventAggregator.prototype.off = function(key, tokenOrCallback) {
 		}
 	} else {
 		//Token was passed in
-		if (this.eventKeys[key] && this.eventKeys[key].subscriptions[tokenOrCallback]) {
-			delete this.eventKeys[key].subscriptions[tokenOrCallback];
+		if (this.eventKeys[compoundKey] && this.eventKeys[compoundKey].subscriptions[tokenOrCallback]) {
+			delete this.eventKeys[compoundKey].subscriptions[tokenOrCallback];
 		}
 	}
 };
 
 EventAggregator.prototype.trigger = function(key) {
 	var self = this;
-	if (self.eventKeys[key]) {
+	var compoundKey = this.instanceId ? this.instanceId + key : key;		
+	if (self.eventKeys[compoundKey]) {
 		var values = Array.prototype.slice.call(arguments, 1);
 		//If passing less than values pass them individually
 		var a1 = values[0],
@@ -57,7 +61,7 @@ EventAggregator.prototype.trigger = function(key) {
 			a1 = values;
 		}
 
-		var subscriptions = self.eventKeys[key].subscriptions;
+		var subscriptions = self.eventKeys[compoundKey].subscriptions;
 		setTimeout(function() {
 			for (var token in subscriptions) {
 				if (subscriptions.hasOwnProperty(token)) {
